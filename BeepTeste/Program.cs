505fcc1f973b9b5ce23822252306480885cd8750
Alisson Schelbauer <alisson_schelbauer@hotmail.com>
2019-07-15 09:50:54 -0300

    Notas em 3 oitavas

--- BeepTeste/Program.cs
+++ BeepTeste/Program.cs
@@ -11,12 +11,33 @@ namespace BeepTeste
     {
         static void Main(string[] args)
         {
-
+            #region notas
+            var C1 = 130;
+            var Csus1 = 138;
+            var D1 = 146;
+            var Dsus1 = 155;
+            var E2 = 164;
+            var F2 = 174;
+            var Fsus2 = 185;
+            var G2 = 196;
+            var Gsus2 = 207;
+            var A2 = 220;
+            var Asus2 = 466;
             var B2 = 493;
+            var C3 = 523;
+            var Csus3 = 554;
             var D3 = 587;
+            var Dsus3 = 622;
             var E3 = 659;
-            var G3 = 784;
+            var F3 = 698;
             var Fsus3 = 740;
+            var G3 = 784;
+            var Gsus3 = 830;
+            var A3 = 880;
+            var Asus3 = 932;
+            var B3 = 987;
+            #endregion
+
 
             Console.Beep(B2, 800);
             Console.Beep(D3, 800);
@@ -32,10 +53,10 @@ namespace BeepTeste
             Console.Beep(G3, 200);
             Console.Beep(Fsus3, 200);
 
-            Console.Beep(587, 400);
-            Console.Beep(587, 400);
-            Console.Beep(659, 400);
-            Console.Beep(659, 800);
+            Console.Beep(D3, 400);
+            Console.Beep(D3, 800);
+            Console.Beep(E3, 400);
+            Console.Beep(E3, 800);
 
 
 
---
+++ ListaBiEasy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaBiEasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeNome = new string[5, 2];
+
+            CarregaInformacoesEListaNaTela(ref listaDeNome);
+
+            PesquisaNaLista(ref listaDeNome, pID);
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Metodo que carrega informações e lista na tela.
+        /// </summary>
+        /// <param name="arrayBi"></param>
+        public static void CarregaInformacoesEListaNaTela(ref string[,] arrayBi)
+        {
+            for (int i = 0; i < arrayBi.GetLength(0); i++)
+            {
+                arrayBi[i, 0] = i.ToString();
+                arrayBi[i, 1] = $"Alisson_{i}";
+            }
+
+            for (int i = 0; i < arrayBi.GetLength(0); i++)
+            {
+                Console.WriteLine($"ID: {arrayBi[i, 0]} - Nome: {arrayBi[i, 1]}");
+            }
+        }
+
+        public static void PesquisaNaLista(ref string[,] arrayBi, string pID)
+        {
+            for (int i = 0; i < arrayBi.GetLength(0); i++)
+            {
+                pID = Console.ReadLine();
+                if (arrayBi[i,0] == pID)
+                {
+                    Console.WriteLine($"Informação escolhida: ID: {arrayBi[i,0]} - Nome{arrayBi[i,1]}");
+                    return;
+                }
+            }
+            Console.WriteLine("Infelizmente a busa pelo ID não resultou em nenhuma informação;");
+        }
+
+    }
+}
