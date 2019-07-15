3e3e1c22f46cb4a614f880f6ca9a8f14ecd7fe71
Alisson Schelbauer <alisson_schelbauer@hotmail.com>
2019-07-15 13:29:33 -0300

    Começo parte 2

--- BeepStaiwayToHeaven/Program.cs
+++ BeepStaiwayToHeaven/Program.cs
@@ -10,7 +10,7 @@ namespace BeepStaiwayToHeaven
     {
         static void Main(string[] args)
         {
-
+            
             Console.Beep(440, 600);
             Console.Beep(523, 600);
             Console.Beep(659, 600);
@@ -43,6 +43,21 @@ namespace BeepStaiwayToHeaven
             Console.Beep(392, 600);
             Console.Beep(440, 600);
             Console.Beep(440, 1200);
+            
+            //Parte 2
+
+            Console.Beep(220, 600);
+            Console.Beep(246, 600);
+            Console.Beep(261, 600);
+            Console.Beep(329, 600);
+            Console.Beep(392, 600);
+            Console.Beep(523, 600);
+            Console.Beep(740, 600);
+            Console.Beep(587, 600);
+            Console.Beep(440, 600);
+            Console.Beep(740, 600);
+            Console.Beep(659, 600);
+            Console.Beep(523, 600);
 
         }
     }
