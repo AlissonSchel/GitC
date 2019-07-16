dcfbd06b3f3cdfd70d36a2bdc1426be5e1deef21
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-16 11:59:11 -0300

    Intro incompleta

--- FearOfTheDarkBeep/Program.cs
+++ FearOfTheDarkBeep/Program.cs
@@ -10,33 +10,123 @@ namespace FearOfTheDarkBeep
     {
         static void Main(string[] args)
         {
+            #region notas
+                int C1 = 261;
+                int Csus1 = 277;
+                int D1 = 293;
+                int Dsus1 = 311;
+                int E1 = 329;
+                int F1 = 349;
+                int Fsus1 = 370;
+                int G1 = 392;
+                int Gsus1 = 415;
+                int A1 = 440;
+                int Asus1 = 466;
+                int B1 = 493;
 
-            int C1 = 261;
-            int Csus1 = 277;
-            int D1 = 293;
-            int Dsus1 = 311;
-            int E1 = 329;
-            int F1 = 349;
-            int Fsus1 = 370;
-            int G1 = 392;
-            int Gsus1 = 415;
-            int A1 = 440;
-            int Asus1 = 466;
-            int B1 = 493;
+                int C2 = 523;
+                int Csus2 = 554;
+                int D2 = 587;
+                int Dsus2 = 622;
+                int E2 = 659;
+                int F2 = 698;
+                int Fsus2 = 740;
+                int G2 = 784;
+                int Gsus2 = 830;
+                int A2 = 880;
+                int Asus2 = 932;
+                int B2 = 987;
+            #endregion
+            
+            Console.Beep(D2,300);
+            Console.Beep(D2,300);
+            Console.Beep(A1,300);
+            Console.Beep(A1,300);
+            Console.Beep(D2, 300);
+            Console.Beep(D2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(F2, 300);
+            Console.Beep(F2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(D2, 300);
+            Console.Beep(D2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(D2, 300);
+            
 
-            int C2 = 523;
-            int Csus2 = 554;
-            int D2 = 587;
-            int Dsus2 = 622;
-            int E2 = 659;
-            int F2 = 698;
-            int Fsus2 = 740;
-            int G2 = 784;
-            int Gsus2 = 830;
-            int A2 = 880;
-            int Asus2 = 932;
-            int B2 = 987;
+            Console.Beep(C2,300);
+            Console.Beep(C2,300);
+            Console.Beep(G1,300);
+            Console.Beep(G1,300);
+            Console.Beep(C2, 300);
+            Console.Beep(C2, 300);
+            Console.Beep(D2, 300);
+            Console.Beep(D2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(D2, 300);
+            Console.Beep(D2, 300);
+            Console.Beep(C2, 300);
+            Console.Beep(C2, 300);
+            Console.Beep(E2, 300);
+            Console.Beep(C2, 500);
+            
+           
 
+
+
+            Console.Beep(Gsus1,300);
+            Console.Beep(Gsus1,300);
+            Console.Beep(Dsus1,300);
+            Console.Beep(Dsus1,300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(B1, 300);
+            Console.Beep(B1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Gsus1, 300);
+            
+            Console.Beep(Fsus1,300);
+            Console.Beep(Fsus1,300);
+            Console.Beep(Csus1,300);
+            Console.Beep(Csus1,300);
+            Console.Beep(Fsus1, 300);
+            Console.Beep(Fsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Fsus1, 300);
+            Console.Beep(Fsus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Fsus1, 300);
+
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Dsus1, 300);
+            Console.Beep(Dsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(B1, 300);
+            Console.Beep(B1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Gsus1, 300);
+            Console.Beep(Asus1, 300);
+            Console.Beep(Gsus1, 300);
         }
     }
 }
