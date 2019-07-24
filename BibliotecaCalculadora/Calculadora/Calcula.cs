ef53b78e54ebbff5cb1127950671100a31ce7f8c
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 16:55:45 -0300

    Calculadora

---
+++ BibliotecaCalculadora/Calculadora/Calcula.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCalculadora.Calculadora
+{
+    public class Calcula
+    {
+        public void Somar()
+        {
+            Console.WriteLine("Informe o primeiro valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor1);
+
+            Console.WriteLine("Informe o segundo valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor2);
+
+            int Resultado = Valor1 + Valor2;
+
+            Console.WriteLine($"Resultado da soma: {Resultado}\n");
+            Console.ReadKey();
+        }
+
+        public void Subtrair()
+        {
+            Console.WriteLine("Informe o primeiro valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor1);
+
+            Console.WriteLine("Informe o segundo valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor2);
+
+            int Resultado = Valor1 - Valor2;
+
+            Console.WriteLine($"Resultado da subtração: {Resultado}\n");
+            Console.ReadKey();
+        }
+
+        public void Multiplicar()
+        {
+            Console.WriteLine("Informe o primeiro valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor1);
+
+            Console.WriteLine("Informe o segundo valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor2);
+
+            int Resultado = Valor1 * Valor2;
+
+            Console.WriteLine($"Resultado da multiplicação: {Resultado}\n");
+            Console.ReadKey();
+        }
+
+        public void Dividir()
+        {
+            Console.WriteLine("Informe o primeiro valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor1);
+
+            Console.WriteLine("Informe o segundo valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor2);
+
+            int Resultado = Valor1 / Valor2;
+
+            Console.WriteLine($"Resultado da dividisão: {Resultado}\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BibliotecaCalculadora/Program.cs
@@ -0,0 +1,32 @@
+﻿using BibliotecaCalculadora.Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Calculadora = new Calcula();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Informe a operação desejada: (1) Soma | (2) Subtração | (3) Multiplicação | (4) Dvisão");
+                var opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1": { Calculadora.Somar(); } break;
+                    case "2": { Calculadora.Subtrair(); } break;
+                    case "3": { Calculadora.Multiplicar(); } break;
+                    case "4": { Calculadora.Dividir(); } break;
+                }
+            }
+            
+        }
+    }
+}
