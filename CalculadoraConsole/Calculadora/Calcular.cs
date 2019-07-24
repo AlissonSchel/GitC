3fb160fe12039d8d4426dc75de666a1f79d2826d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 17:20:54 -0300

    Calculadora via Class 2.0

---
+++ CalculadoraConsole/Calculadora/Calcular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraConsole.Calculadora
+{
+    public class Calcular
+    {
+        public void Calculadora()
+        {
+            Console.WriteLine("Informe o primeiro valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor1);
+
+            Console.WriteLine("Informe o segundo valor: ");
+            int.TryParse(Console.ReadLine(), out int Valor2);
+
+            Console.WriteLine("Informe a operação: ");
+            string operacao = Console.ReadKey().KeyChar.ToString();
+
+            int Resultado;
+
+                Console.Clear();
+                switch (operacao)
+                {
+                    case "+": { Resultado = Valor1 + Valor2; Console.WriteLine($"Resultado da soma: {Resultado}"); Console.ReadKey(); } break;
+                    case "-": { Resultado = Valor1 - Valor2; Console.WriteLine($"Resultado da subtração: {Resultado}"); Console.ReadKey(); } break;
+                    case "*": { Resultado = Valor1 * Valor2; Console.WriteLine($"Resultado da multiplicação: {Resultado}"); Console.ReadKey(); } break;
+                    case "/": { Resultado = Valor1 / Valor2; Console.WriteLine($"Resultado da divisão: {Resultado}"); Console.ReadKey(); } break;
+                    case "0": { Environment.Exit(0); } break;
+                    default: { Console.WriteLine("Operação inválida..."); Console.Clear(); } break;
+                }
+            Console.Clear();
+        }
+    }
+}
---
+++ CalculadoraConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculadoraConsole.Calculadora;
+
+namespace CalculadoraConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Calcula = new Calcular();
+
+            while (true)
+            {
+                Calcula.Calculadora();
+            } 
+        }
+    }
+}
