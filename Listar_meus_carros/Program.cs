98aa535e300bfd433520a81442c939ae868f51d6
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 17:22:00 -0300

    List Carros

---
+++ Listar_meus_carros/Program.cs
@@ -0,0 +1,72 @@
+﻿using Listar_meus_carros.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listar_meus_carros
+{
+    class Program
+    {
+        static List<Carro> listaCarros = new List<Carro>();
+        static void Main(string[] args)
+        {   
+            while (true)
+            {
+                
+                switch (MostrarMenu())
+                {
+                    case 1: { AdicionarCarro(); break; }
+                    case 2: { ListaCarros(); break; }
+                    case 3: { Environment.Exit(0); break; }
+                    default: { Console.WriteLine("Opção inválida."); break; }
+                }
+            }
+        }
+
+        public static int MostrarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Selecione a opção desejada: \n");
+
+            Console.WriteLine("(1) - Adicionar Carro");
+            Console.WriteLine("(2) - Listar Carros");
+            Console.WriteLine("(3) - Sair");
+            int.TryParse(Console.ReadKey().KeyChar.ToString(), out int opcao);
+
+            return opcao;
+        }
+
+        public static void AdicionarCarro()
+        {
+            Console.Clear();
+            listaCarros.Add(new Carro()
+            {
+                Modelo = InsereValor("modelo"),
+                Ano = int.Parse(InsereValor("ano")),
+                Placa = InsereValor("placa"),
+                Cv = int.Parse(InsereValor("Cv"))
+            });
+
+            Console.WriteLine("Carro adicionado com sucesso.");
+            Console.ReadKey();
+        }
+
+        public static void ListaCarros()
+        {
+            Console.Clear();
+            foreach (Carro item in listaCarros)
+            {
+                Console.WriteLine($"Modelo: {item.Modelo} | Ano: {item.Ano} | Placa: {item.Placa} | Cv: {item.Cv}");
+            }
+            Console.ReadKey();
+        }
+
+        public static string InsereValor(string campo)
+        {
+            Console.WriteLine($"Informe o {campo} do carro.");
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ PrimeiraListaTipada/Program.cs
@@ -0,0 +1,70 @@
+﻿using PrimeiraListaTipada.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraListaTipada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Lanche> minhaLista = new List<Lanche>();
+
+            minhaLista.Add(new Lanche()
+            {
+                 Nome = "Pão de Queijo",
+                 Quantidade = 9,
+                 Valor = 1.85
+            });
+
+            minhaLista.Add(new Lanche()
+            {
+                Nome = "Bolinho de Soya + Pepsi",
+                Quantidade = 2,
+                Valor = 7.00
+            });
+
+            bool adicionarLanche = true;
+            while (adicionarLanche)
+            {
+                Console.Clear();
+                Console.WriteLine("Informe o nome do lanche.");
+                var nomeLanche = Console.ReadLine();
+                Console.WriteLine($"Informe a quantidade de {nomeLanche}");
+                int.TryParse(Console.ReadLine(), out int quantidade);
+                Console.WriteLine($"Informe o valor de {nomeLanche}");
+                double.TryParse(Console.ReadLine(), out double valor);
+
+                minhaLista.Add(new Lanche()
+                {
+                    Nome = nomeLanche,
+                    Quantidade = quantidade,
+                    Valor = valor
+                });
+                Console.Clear();
+                Console.WriteLine("Lanche adicionado com sucesso.\n");
+
+                Console.WriteLine("Deseja adicionar mais lanches?");
+                Console.WriteLine("(1) - Sim | (2) Não | (3) - Ver Lista de Lanches\n");
+                int.TryParse(Console.ReadLine(), out int opcao);
+
+                if (opcao == 2)
+                {
+                    adicionarLanche = false;
+                } if (opcao == 3)
+                {
+                    Console.Clear();
+                    foreach (Lanche item in minhaLista)
+                    {
+                        Console.WriteLine($"Lanches disponíveis: {item.Nome} | Quantidade: {item.Quantidade} | Preço: {item.Valor}");
+                    }
+                    Console.ReadKey();
+                    adicionarLanche = true;
+                }
+            }
+        }
+    }
+}
